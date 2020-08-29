b26b15d9397f7f4f7bbd6f873b6c94f35f2ceacf
Maku <Maku@LAPTOP-147I866T>
2020-08-29 17:49:50 +0800

    started manage inventory & add product functions

--- controller/indexController.js
+++ controller/indexController.js
@@ -104,9 +104,124 @@ const indexFunctions = {
 		if (!adminInsert){
 			//error handling
 		} else {
-			//send success status or render/redirect to page
+			//send success status or render or redirect to page
 		}
 		res.redirect('/');
+	},
+
+/* Update Order Status --
+ * 
+ * When items are shipped, details regarding their delivery will be sent 
+ * through the buyer’s email and displayed in the view order status page, 
+ * by utilizing the tracking details from the partner courier. When items 
+ * are cancelled, the reason for cancelling will also be displayed.
+ */
+
+/** Manage Inventory --
+ * 
+ * The admin/seller can add products or edit existing products 
+ * with their corresponding pictures, details and colors. Since 
+ * pre-order takes place, every order made will show up in the 
+ * inventory system (like number of items per product needed) 
+ * on the admin’s side.
+ * 
+ * SELECT *
+ * FROM Product p
+ * JOIN ProdCategory pc
+ * ON p.productID = pc.productID
+ * JOIN ProdPhoto pp
+ * ON p.productID = pp.productID
+ * WHERE p.productID = req.query.text
+ */
+
+	getJoinedQuery: async function(req, res){
+		var query = await db.aggregate('Product', [
+			{'$match': {productID: req.query.text}},
+			{'$lookup': {
+				'from': 'ProdCategory',
+				'localField': 'productID',
+				'foreignField': 'productID',
+				'as': 'prodCateg'
+			}},
+			{'$lookup': {
+				'from': 'ProdPhoto',
+				'localField': 'productID',
+				'foreignField': 'productID',
+				'as': 'prodPhoto'
+			}}
+		]);
+		
+		if (!query){
+			//handle error
+		} else {
+			return res;
+		}
+	},
+	
+	addProduct: async function(req, res){
+		try {
+			var product = await getJoinedQuery();
+
+			// how to deal with adding quantity?
+			let {productID, name, price, size, color, categName, photoLink} = req.body;
+
+			var prodFind = await db.findOne(Product, {productID: productID});
+
+			if (prodFind){
+				// handle error: product exists in db
+			} else {
+				var prodInsert = await db.insertOne('Product', {productID: productID});
+				var categInsert = await db.insertOne('ProdCategory', {productID: productID});
+				var photoInsert = await db.insertOne('ProdPhoto', {productID: productID});
+			}
+		} catch (e){
+			// error handling
+		}
+	},
+	
+	editProduct: async function(req, res){
+		try {
+			var product = await getJoinedQuery();
+
+			// how to deal with updating prod quantity?
+			let {productID, name, price, size, color, categName, photoLink} = req.body;
+
+			var prodFind = await db.findOne(Product, {productID: productID});
+
+			if (!prodFind){
+				// handle error: cannot edit product that does not exist
+			} else {
+				//update the doc
+			}	
+		} catch (e){
+			// error handling
+		}
+	},
+	
+	
+/** The admin may choose to create a new category that products may be labelled under.
+ */
+	addProductCateg: async function(req, res){
+		let {categName} = req.body;
+		
+		var categFind = await db.findOne(Category, {categName: categName});
+		
+		if (categFind){
+			// handle error: category exists in db
+		} else {
+			var categInsert = await db.insertOne(Category, {categName: categFind});
+			
+			if (!categInsert){
+				// handle error
+			} else {
+				//categ added; redirect to page
+			}
+		}
+		
+		//when and what is the flow to add products in this category? --make as separate function
+		//retrieve list of products
+		//ask admin to choose which to put in categ
+		
 	}
 
 };
