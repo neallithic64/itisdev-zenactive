771fcfac2f27cbe3bd7bb84c67af40beb16f374c
Neal Lim <55176465+neallithic64@users.noreply.github.com>
2020-08-30 02:44:02 +0800

    Drafting more frontend scripts - add to cart - view cart - initial check out cart

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,36 @@
 
 $(document).ready(function() {
-	if (!window.sessionStorage.getItem('cart')) {
-		window.sessionStorage.setItem('cart', []);
+	// creating a new cart
+	if (!getSessionCart()) {
+		window.sessionStorage.setItem('cart', JSON.stringify([]));
 	}
 	
+	$("submitCart idk what to put here").click(() => {
+		var cart = getSessionCart();
+		$.post('/checkout', cart, result => {
+			// idk
+		});
+	});
 	
+	if (location.href === "viewCart idk exactly yet") {
+		getSessionCart().forEach(e => {
+			$("select something here idk yet").append(e);
+		});
+	}
 });
+
+function getSessionCart() {
+	return JSON.parse(window.sessionStorage.getItem('cart'));
+}
+
+function addToCart(s) {
+	var cart = getSessionCart();
+	cart.push(s);
+	window.sessionStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function removeFromCart(s) {
+	var cart = getSessionCart();
+	cart = cart.filter(e => e !== s);
+	window.sessionStorage.setItem('cart', JSON.stringify(cart));
+}
