7b870cbf6397d101937e367ea52b35e299d8f2f8
HP <HP@HP_PC.bbrouter>
2020-10-03 15:43:22 +0800

    working <post shipTrackID>

--- router/indexRouter.js
+++ router/indexRouter.js
@@ -67,6 +67,7 @@ router.post('/addProduct', adminCont.postAddProduct);
 router.post('/addCategory', adminMiddle.validateAddCateg, adminCont.postAddCateg);
 router.post('/updateSalesStatus', adminMiddle.validateUpdateSalesOrd, adminCont.postUpdateSalesOrder);
 router.post('/updatePurchStatus', adminMiddle.validateUpdatePurchOrd, adminCont.postUpdatePurchOrder);
+router.post('/addTrackID', adminCont.postTrackID);
 
 router.post('/editProduct/:id', adminMiddle.validateProductExist, adminCont.postEditProduct);
 router.post('/addProdCateg/:id', adminMiddle.validateProductExist, adminMiddle.validateAddProdCateg, adminCont.postAddProdCateg);
